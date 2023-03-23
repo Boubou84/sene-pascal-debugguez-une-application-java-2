4203db91d7231d6282ecfaffd982f46fdcdc3347
Pascal Sene <wtp2t8wz54g9-student@anonymous-codio.com>
2023-03-23 14:23:52 +0000

    writer implementation

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -3,7 +3,6 @@ package com.hemebiotech.analytics;
 import java.util.Map;
 /**
  * Anything that will write symptom data on a valid data output format
- *
  */
 public interface ISymptomWriter {
 /**
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,31 +1,40 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
-public class WriteSymptomsDataToFile implements ISymptomWriter {
+public class WriteSymptomDataToFile implements ISymptomWriter {
 
-  private String filepath;
- /**
- * @param filepath A complete or partial path to a file where the symptoms and their
- *                 quantities will be written, one per line
- * 
- */
-public WriteSymptomsDataToFile(String filepath) {
-    this.filepath = filepath;
-  }
+    private String filepath;
 
-@Override
-public void writeSymptoms(Map<String, Integer> symptoms) {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
-      for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
-        writer.write(entry.getKey() + ": " + entry.getValue());
-        writer.newLine();
-      }
-    }catch (IOException e) {
-      System.err.println("Error while writing symptoms to file : " + e.getMessage());
+    /**
+     * Default constructor with a default file path.
+     */
+    public WriteSymptomDataToFile() {
+        this.filepath = "default_output_path.txt";
     }
-  }
+
+    /**
+     * @param filepath A complete or partial path to a file where the symptoms and their
+     *                 quantities will be written, one per line
+     */
+    public WriteSymptomDataToFile(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        if (filepath != null && symptoms != null) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+                for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                    writer.write(entry.getKey() + ": " + entry.getValue());
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                System.err.println("Error while writing symptoms to file : " + e.getMessage());
+            }
+        }
+    }
 }
