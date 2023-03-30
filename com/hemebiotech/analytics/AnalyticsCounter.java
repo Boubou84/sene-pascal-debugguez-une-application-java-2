ee09ccdf341ea6e527df55a8b27706f7550c3fe8
Pascal Sene <wtp2t8wz54g9-student@anonymous-codio.com>
2023-03-30 11:05:18 +0000

    Création de la méthode getSymptoms qui récupere la liste des entrées-de la méthode count qui compte les occurrences-de la méthod sort qui tri symtômes et occurrences par ordre alphabétique-et la méthode writeSymptoms qui écrit les résultat dans le fichier result.out

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,41 +2,59 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
-	
-	public static void main(String args[]) throws Exception {
-		
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
 
-		int i = 0;	
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    public List<String> getSymptoms() {
+        return reader.getSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> symptomCounts = new HashMap<>();
+        for (String symptom : symptoms) {
+            symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
+        }
+        return symptomCounts;
+    }
+
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        return new TreeMap<>(symptoms);
+    }
+
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    }
+
+    public static void main(String[] args) {
+        String inputPath = "symptoms.txt";
+        String outputPath = "result.out";
+        ISymptomReader reader = new ReadSymptomDataFromFile(inputPath);
+        ISymptomWriter writer = new WriteSymptomDataToFile(outputPath);
+        AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+        List<String> symptoms = counter.getSymptoms();
+        System.out.println("Les symptômes: " + symptoms);
+
+        Map<String, Integer> symptomCounts = counter.countSymptoms(symptoms);
+        System.out.println("Comptent les symptômes: " + symptomCounts);
+
+        Map<String, Integer> sortedSymptomCounts = counter.sortSymptoms(symptomCounts);
+        System.out.println("Nombre de symptômes triés: " + sortedSymptomCounts);
+
+        counter.writeSymptoms(sortedSymptomCounts);
+        System.out.println("Symptômes écrits dans le fichier de sortie");
+    }
 }
